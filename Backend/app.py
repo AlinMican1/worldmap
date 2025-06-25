452015d7d97e3b0e0e2b5896a9232502641a557d
AlinMican <AlinMican2001@gmail.com>
2025-06-25 20:17:10 +0100

    Works on locations.

--- Backend/app.py
+++ Backend/app.py
@@ -1,9 +1,16 @@
-from flask import Flask
+from flask import Flask, jsonify
+from flask_cors import CORS
+from os import environ
+
 app = Flask(__name__)
+CORS(app) #Enable CORS to allow to HTTP requests from different ports.
 
 @app.route('/api/hello', methods=['GET'])
 def hello_world():
-    return "Hello, World!"
+    return jsonify({
+        "message": "Hello World",
+        "statuscode": 200
+    })
 
 if __name__ == '__main__':
-    app.run(port=5328)
+    app.run(debug=True,port=8080) #port=environ.get(BASE_PORT))
