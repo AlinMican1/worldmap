00f8eb296a7e724fffe4022cd0a13790cd78da2a
AlinMican <AlinMican2001@gmail.com>
2025-06-26 22:25:28 +0100

    adds suggest location search.

--- Frontend/src/components/atoms/inputLocation.tsx
+++ Frontend/src/components/atoms/inputLocation.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import { useState } from "react";
+import { SuggestLocation } from "../../../helper/SuggestLocation";
+
 const InputLocation = () => {
   const [inputValue] = useState<Array<string>>([]);
   const [location, setLocation] = useState<string>("");
@@ -20,14 +22,21 @@ const InputLocation = () => {
           value={location}
           onChange={(event) => setLocation(event.target.value)}
         />
+        <ul>
+          {SuggestLocation(location).map((place, key) => (
+            // eslint-disable-next-line react/jsx-key
+            <p key={key + 1}>{place}</p>
+          ))}
+        </ul>
+
         <button>Submit</button>
       </form>
 
-      <ul>
-        {inputValue.map((place, key) => (
-          <p key={key + 1}>{place}</p>
-        ))}
-      </ul>
+      {/* <ul>
+         {inputValue.map((place, key) => (
+           <p key={key + 1}>{place}</p>
+         ))}
+       </ul> */}
     </div>
   );
 };
