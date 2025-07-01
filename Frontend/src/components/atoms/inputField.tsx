dd3a8b03365e48b71ae6c50b5adb7eebe3c3ee5e
AlinMican <AlinMican2001@gmail.com>
2025-07-01 22:14:31 +0100

    works on form and input box.

---
+++ Frontend/src/components/atoms/inputField.tsx
@@ -0,0 +1,56 @@
+import { ChangeEvent } from "react";
+import "./inputField.css";
+
+interface inputFieldParams {
+  type?: "text" | "number" | "email" | "password";
+  label?: string;
+  id?: string;
+  value?: string | number;
+  name?: string;
+  placeholder?: string;
+  error?: boolean;
+  errorMsg?: string;
+  disabled?: boolean;
+  autocomplete?: string;
+  onChange?: (e: ChangeEvent<HTMLInputElement>) => void;
+  icon?: React.ReactNode;
+}
+
+export const InputField = ({
+  type,
+  label,
+  id,
+  value,
+  name,
+  placeholder,
+  error,
+  disabled,
+  errorMsg,
+  autocomplete,
+  onChange,
+  icon,
+}: inputFieldParams) => {
+  return (
+    <div className="input-wrapper">
+      <label htmlFor={label}>
+        {label}
+      </label>
+      <div className="input-with-icon">
+        {icon && <span className="input-icon">{icon}</span>}{" "}
+        {/* Render the icon if provided */}
+        <input
+          type={type}
+          id={id}
+          value={value}
+          name={name}
+          autoComplete={autocomplete}
+          placeholder={placeholder}
+          onChange={onChange}
+          disabled={disabled}
+          className={`${error ? "input-error" : ""}`}
+        />
+      </div>
+      {error && <p className={`${"error"}`}>{errorMsg}</p>}
+    </div>
+  );
+};
