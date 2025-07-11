971623d7ad3f2a0690bc2bea0e1f2b14ca6b278c
alin Mican <alinmican2001@gmail.com>
2025-07-11 22:35:32 +0100

    Adds custom hook for client form.

--- Frontend/src/hooks/useClientForm.ts
+++ Frontend/src/hooks/useClientForm.ts
@@ -1,3 +1,26 @@
-const useClientForm = () => {};
+import { ChangeEvent, useState } from "react";
+const useClientForm = <T extends Record<string, any>>(initialValues: T) => {
+  const [formData, setFormData] = useState<T>(initialValues);
+
+  const handleChange = (
+    e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement | HTMLSelectElement>
+  ) => {
+    const { name, value } = e.target;
+
+    setFormData((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
+  };
+
+  const resetFormData = () => setFormData(initialValues);
+
+  return {
+    resetFormData,
+    handleChange,
+    formData,
+    setFormData,
+  };
+};
 
 export default useClientForm;
