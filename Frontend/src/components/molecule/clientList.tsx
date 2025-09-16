96f7cfb09ef3fcda624205fc41e8ca053b702339
alin Mican <alinmican2001@gmail.com>
2025-09-16 19:33:01 +0100

    finalises participant component.

--- Frontend/src/components/molecule/clientList.tsx
+++ Frontend/src/components/molecule/clientList.tsx
@@ -2,13 +2,13 @@
 import BoxDesign from "../atoms/boxDesign";
 import "./clientList.css";
 import "../../app/globals.css";
-import { AddClientInfoProps, ClientInfoProps, ClientListProps } from "@/types/interfaces";
+import { AddClientInfoProps, ClientInfoProps } from "@/types/interfaces";
 import UserPfp from "../atoms/userPfp";
 import Button from "../atoms/button";
-import SelectedParticipants from "./selectedParticipants";
 import PlusIcon from "../icons/plus";
-
+import { memo } from "react";
 const ClientList = ({ clients, setClients }: AddClientInfoProps) => {
+  console.log("CLIENTS LIST RENDERED");
   const handleSelectedParticipants = (participant: ClientInfoProps) => {
     setClients((old) => old.map((c) => (c === participant ? { ...c, selected: !c.selected } : c)));
   };
@@ -24,7 +24,7 @@ const ClientList = ({ clients, setClients }: AddClientInfoProps) => {
               <div className="elements-column-no-gap">
                 <div className="elements-row">
                   <h3 className="client-name">
-                    {client.first_name.slice(0, 15)} {client.surname}
+                    {client.first_name} {client.surname}
                   </h3>
                   <p className="email-title"> • {client.email}</p>
                 </div>
@@ -41,4 +41,4 @@ const ClientList = ({ clients, setClients }: AddClientInfoProps) => {
     </div>
   );
 };
-export default ClientList;
+export default memo(ClientList);
