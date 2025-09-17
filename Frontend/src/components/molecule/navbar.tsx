ea61bab2e7cd062f8260b59cc3d5761ce43d0fd7
alin Mican <alinmican2001@gmail.com>
2025-09-17 20:22:38 +0100

    Works on nav bar buttons.

--- Frontend/src/components/molecule/navbar.tsx
+++ Frontend/src/components/molecule/navbar.tsx
@@ -1,8 +1,37 @@
 "use client";
 import { usePathname } from "next/navigation";
-
+import "./navbar.css";
+import BoxDesign from "../atoms/boxDesign";
+import Button from "../atoms/button";
+import { useEffect, useState } from "react";
+import Link from "next/link";
 const Navbar = () => {
   const pathname = usePathname();
-  return <>{pathname !== "/" && <div>NAVBAR</div>}</>;
+
+  const [activePage, setActivePage] = useState<string>("");
+
+  return (
+    <>
+      {/* {pathname !== "/something" && ( */}
+      <div className="navbar-container">
+        <h1>GLOBAL MEET</h1>
+        <BoxDesign variant="secondary-DesignBox" centered="left" orientation="column">
+          <p>NAVIGATION</p>
+          <Link href="/" passHref>
+            <button className={pathname === "/" ? "active" : ""}>Home</button>
+          </Link>
+
+          <Link href="/schedule" passHref>
+            <Button variant={pathname === "/schedule" ? "nav-btn" : "nav-btn"}>
+              <div className="elements-column">
+                Schedule Meeting <p>Create a schedule</p>
+              </div>
+            </Button>
+          </Link>
+        </BoxDesign>
+      </div>
+      {/* )} */}
+    </>
+  );
 };
 export default Navbar;
