b840b593c82cbfa65c81bdd95da73edd3c1834e3
alin Mican <alinmican2001@gmail.com>
2025-09-18 19:49:12 +0100

    works on log out button.

---
+++ Frontend/src/components/icons/logout.tsx
@@ -0,0 +1,23 @@
+import { IconProps } from "@/types/interfaces";
+
+const LogoutIcon = ({ className }: IconProps) => {
+  return (
+    <svg
+      className={className}
+      width="16"
+      height="16"
+      viewBox="0 0 24 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M14 20H6C4.89543 20 4 19.1046 4 18L4 6C4 4.89543 4.89543 4 6 4H14M10 12H21M21 12L18 15M21 12L18 9"
+        stroke="currentColor"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+};
+export default LogoutIcon;
