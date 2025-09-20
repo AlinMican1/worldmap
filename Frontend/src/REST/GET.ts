3f7befb32cdcacf66222e742e5d9b508dc5d7917
alin Mican <alinmican2001@gmail.com>
2025-09-20 20:05:54 +0100

    Bug fixes + adding quick stats component in nav bar.

--- Frontend/src/REST/GET.ts
+++ Frontend/src/REST/GET.ts
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 export const GetParticipants = async (id: string) => {
+  console.log("GET CLIENT");
   try {
     const response = await axios.get(`${process.env.NEXT_PUBLIC_DEV_URL}getParticipants/${id}`);
     return response.data.participants;
--- Frontend/src/components/icons/clock.tsx
+++ Frontend/src/components/icons/clock.tsx
@@ -12,7 +12,7 @@ const ClockIcon = ({ className }: IconProps) => {
     >
       <path
         d="M12 7V12H15M21 12C21 16.9706 16.9706 21 12 21C7.02944 21 3 16.9706 3 12C3 7.02944 7.02944 3 12 3C16.9706 3 21 7.02944 21 12Z"
-        stroke="#000000"
+        stroke="currentColor"
         strokeWidth="2"
         strokeLinecap="round"
         strokeLinejoin="round"
