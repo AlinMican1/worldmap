877a3667dbacb278792393d42bd2a350a7d6cfc6
alin Mican <alinmican2001@gmail.com>
2025-10-05 22:06:04 +0100

    works on layout.

--- Frontend/src/components/organism/addClientInfo.tsx
+++ Frontend/src/components/organism/addClientInfo.tsx
@@ -1,6 +1,7 @@
 "use client";
 import BoxDesign from "../atoms/boxDesign";
 import "../../app/globals.css";
+import "./addClientInfo.css";
 import { InputField } from "../atoms/inputField";
 import { ErrorMessageProps, ClientInfoProps, AddClientInfoProps } from "@/types/interfaces";
 import { SubmitClientSchedule } from "@/REST/POST";
@@ -149,148 +150,163 @@ const AddClientInfo = ({ clients, setClients }: AddClientInfoProps) => {
         subheading="Create a meeting that works for everyone worldwide"
       />
       <form id="client-form" onSubmit={handleSubmit}>
-        <div className="elements-row">
-          <BoxDesign centeredY="leftY" centeredX="leftX" variant="sixth-DesignBox">
-            <Title
-              variant="secondary"
-              title="Meeting Details"
-              icon={<NotesIcon className="title-icon" size="24" />}
-            />
-            <DateAndTimeContext.Provider value={dateAndTime}>
-              <div className="elements-row">
-                {CalendarBoxMemo}
-                <ChooseTime />
-              </div>
-              <DateAndTimeDisplay />
-            </DateAndTimeContext.Provider>
-          </BoxDesign>
-          {/* <BoxDesign variant="sixth-DesignBox" centeredX="leftX" centeredY="leftY">
-            <Title
-              title="TimeZone Preview"
-              variant="secondary"
-              icon={<EarthIcon className="title-icon" size="28" />}
-            />
-            <PariticipantsPreview
-              clients={clients}
-              setClients={setClients}
-              parentWidth={parentWidth}
-            />
-          </BoxDesign> */}
-        </div>
-        <BoxDesign centeredY="leftY" centeredX="leftX" variant="sixth-DesignBox" ref={boxRef}>
-          <Title
-            title="Select Participants"
-            variant="secondary"
-            icon={<UsersIcon className="title-icon" size="28" />}
-          />
-
-          <SelectedParticipants
-            clients={clients}
-            setClients={setClients}
-            parentWidth={parentWidth}
-          />
-          <ClientList clients={clients} setClients={setClients} />
-
-          <Modal
-            trigger={(open) => (
-              <Button onClick={open}>
-                {" "}
-                Add A New Participant <ArrowRightIcon className="" />{" "}
-              </Button>
-            )}
-          >
-            {(close) => (
-              <BoxDesign
-                variant="third-DesignBox"
-                centeredX="leftX"
-                centeredY="leftY"
-                padding="large"
-              >
-                <Title variant="primary" title="Add New Participant" />
-                <div className="elements-row">
-                  <InputField
-                    autocomplete="off"
-                    type="text"
-                    name="first_name"
-                    label="Person's Name"
-                    value={form.formData.first_name}
-                    id="first_name"
-                    onChange={form.handleChange}
-                    placeholder="Enter Client Name"
-                    error={errorsHook.getErrorBoolean("name")}
-                    errorMsg={errorsHook.getErrorMsg("name")}
-                  />
-                  <InputField
-                    autocomplete="off"
-                    type="text"
-                    name="email"
-                    label="Person's Email"
-                    value={form.formData.email}
-                    id="email"
-                    onChange={form.handleChange}
-                    placeholder="Enter Client Email"
-                    error={errorsHook.getErrorBoolean("email")}
-                    errorMsg={errorsHook.getErrorMsg("email")}
-                  />
-                  <InputField
-                    autocomplete="off"
-                    type="text"
-                    name="surname"
-                    label="Participant Surname"
-                    value={form.formData.surname}
-                    id="participant"
-                    onChange={form.handleChange}
-                    placeholder="Enter Pariticipant Surname"
-                    error={errorsHook.getErrorBoolean("surname")}
-                    errorMsg={errorsHook.getErrorMsg("surname")}
-                  />
-                </div>
-                <div className="elements-row">
-                  <EnterLocation
-                    location={form.formData.location}
-                    setLocation={setLocation}
-                    errorMsg={errorsHook.getErrorMsg("location")}
-                    error={errorsHook.getErrorBoolean("location")}
-                  />
-                  <div>
-                    {timezonesArr.array.length > 0 && (
-                      <div>
-                        <label htmlFor="timezone">Choose a timezone:</label>
-                        <select
-                          name="timezone"
-                          id="timezone"
-                          value={form.formData.timezone || timezonesArr.array[0]}
-                          onChange={(e) =>
-                            form.setFormData((prev) => ({ ...prev, timezone: e.target.value }))
-                          }
-                        >
-                          {timezonesArr.array.map((timezone, idx) => (
-                            <option key={idx} value={timezone}>
-                              {timezone}
-                            </option>
-                          ))}
-                        </select>
-                      </div>
-                    )}
+        <div className="scheduler-layout">
+          <div className="left-column">
+            <div className="calendar-box">
+              <BoxDesign centeredY="leftY" centeredX="leftX" variant="sixth-DesignBox">
+                <Title
+                  variant="secondary"
+                  title="Meeting Details"
+                  icon={<NotesIcon className="title-icon" size="24" />}
+                />
+                <DateAndTimeContext.Provider value={dateAndTime}>
+                  <div className="elements-row">
+                    {CalendarBoxMemo}
+                    <ChooseTime />
                   </div>
-                </div>
-
-                <div className="close-btn-pos">
-                  <button className="close-btn" onClick={() => (close(), form.resetFormData())}>
-                    <ExitIcon className={"exit-icon"} />
-                  </button>
-                </div>
-                <Button
-                  type="button"
-                  variant="secondary-btn"
-                  onClick={(e) => handleAddClients(e, close)}
-                >
-                  Add Clients
-                </Button>
+                  <DateAndTimeDisplay />
+                </DateAndTimeContext.Provider>
               </BoxDesign>
-            )}
-          </Modal>
-        </BoxDesign>
+            </div>
+
+            <div className="participants-section">
+              <BoxDesign centeredY="leftY" centeredX="leftX" variant="sixth-DesignBox" ref={boxRef}>
+                <Title
+                  title="Select Participants"
+                  variant="secondary"
+                  icon={<UsersIcon className="title-icon" size="28" />}
+                />
+
+                <SelectedParticipants
+                  clients={clients}
+                  setClients={setClients}
+                  parentWidth={parentWidth}
+                />
+                <ClientList clients={clients} setClients={setClients} />
+
+                <Modal
+                  trigger={(open) => (
+                    <Button onClick={open}>
+                      {" "}
+                      Add A New Participant <ArrowRightIcon className="" />{" "}
+                    </Button>
+                  )}
+                >
+                  {(close) => (
+                    <BoxDesign
+                      variant="third-DesignBox"
+                      centeredX="leftX"
+                      centeredY="leftY"
+                      padding="large"
+                    >
+                      <Title variant="primary" title="Add New Participant" />
+                      <div className="elements-row">
+                        <InputField
+                          autocomplete="off"
+                          type="text"
+                          name="first_name"
+                          label="Person's Name"
+                          value={form.formData.first_name}
+                          id="first_name"
+                          onChange={form.handleChange}
+                          placeholder="Enter Client Name"
+                          error={errorsHook.getErrorBoolean("name")}
+                          errorMsg={errorsHook.getErrorMsg("name")}
+                        />
+                        <InputField
+                          autocomplete="off"
+                          type="text"
+                          name="email"
+                          label="Person's Email"
+                          value={form.formData.email}
+                          id="email"
+                          onChange={form.handleChange}
+                          placeholder="Enter Client Email"
+                          error={errorsHook.getErrorBoolean("email")}
+                          errorMsg={errorsHook.getErrorMsg("email")}
+                        />
+                        <InputField
+                          autocomplete="off"
+                          type="text"
+                          name="surname"
+                          label="Participant Surname"
+                          value={form.formData.surname}
+                          id="participant"
+                          onChange={form.handleChange}
+                          placeholder="Enter Pariticipant Surname"
+                          error={errorsHook.getErrorBoolean("surname")}
+                          errorMsg={errorsHook.getErrorMsg("surname")}
+                        />
+                      </div>
+                      <div className="elements-row">
+                        <EnterLocation
+                          location={form.formData.location}
+                          setLocation={setLocation}
+                          errorMsg={errorsHook.getErrorMsg("location")}
+                          error={errorsHook.getErrorBoolean("location")}
+                        />
+                        <div>
+                          {timezonesArr.array.length > 0 && (
+                            <div>
+                              <label htmlFor="timezone">Choose a timezone:</label>
+                              <select
+                                name="timezone"
+                                id="timezone"
+                                value={form.formData.timezone || timezonesArr.array[0]}
+                                onChange={(e) =>
+                                  form.setFormData((prev) => ({
+                                    ...prev,
+                                    timezone: e.target.value,
+                                  }))
+                                }
+                              >
+                                {timezonesArr.array.map((timezone, idx) => (
+                                  <option key={idx} value={timezone}>
+                                    {timezone}
+                                  </option>
+                                ))}
+                              </select>
+                            </div>
+                          )}
+                        </div>
+                      </div>
+
+                      <div className="close-btn-pos">
+                        <button
+                          className="close-btn"
+                          onClick={() => (close(), form.resetFormData())}
+                        >
+                          <ExitIcon className={"exit-icon"} />
+                        </button>
+                      </div>
+                      <Button
+                        type="button"
+                        variant="secondary-btn"
+                        onClick={(e) => handleAddClients(e, close)}
+                      >
+                        Add Clients
+                      </Button>
+                    </BoxDesign>
+                  )}
+                </Modal>
+              </BoxDesign>
+            </div>
+          </div>
+          <div className="timezone-preview">
+            <BoxDesign variant="sixth-DesignBox" centeredX="leftX" centeredY="leftY">
+              <Title
+                title="TimeZone Preview"
+                variant="secondary"
+                icon={<EarthIcon className="title-icon" size="28" />}
+              />
+              <PariticipantsPreview
+                clients={clients}
+                setClients={setClients}
+                parentWidth={parentWidth}
+              />
+            </BoxDesign>
+          </div>
+        </div>
       </form>
 
       <BoxDesign padding="none" centeredX="leftX" centeredY="leftY" variant="fifth-DesignBox">
